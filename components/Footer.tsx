bc985ac466f1c8bb839b6054a3871ccef6fe24ed
eliasbelzberg <ebelz@uw.edu>
2025-04-14 00:59:16 -0700

    clean up

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Footer: React.FC = () => {
   return (
